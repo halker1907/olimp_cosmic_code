e33b8aea5ef54532fa96c556d22de0ccc57c8854
halker1907 <113712157+halker1907@users.noreply.github.com>
2024-04-25 19:47:18 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,40 +1,63 @@
 import os
 import csv
+from termcolor import colored
 
-# files_directory = input("Выберете ваш путь к файлам")
 files_directory = r'C:\Users\IT\Desktop\Архив'
-
 print('читаем файлы из', files_directory)
 files_names = os.listdir(files_directory)
-print('Вдиректории найдены файлы:', *files_names)
+print('В директории найдены файлы:', *files_names)
 
-files_name = [
-    file_name for file_name in files_names if file_name.endswith('.csv')
-]
+headers_template = {
+    'id': 0,
+    'name': 1,
+    'surname': 2,
+    'age': 3,
+    'height': 4,
+    'weight': 5,
+    'eyesight': 6,
+    'education': 7,
+    'english_language': 8
+}
 
-print('Работаем с файлами:', *files_names)
-
-headers_template = [
-        'id',
-        'name',
-        'surname',
-        'age',
-        'height',
-        'weight',
-        'eyesight',
-        'education',
-        'english_language'
-    ]
+colors = ['green', 'magenta', 'blue', 'yellow', 'cyan', 'red', 'white', 'green', 'magenta']
 
 for file_name in files_names:
     file_path = os.path.join(files_directory, file_name)
+    if not file_name.endswith('.csv'):
+        continue
     with open(file_path, 'r', encoding='UTF-8') as file:
         reader = csv.reader(file, delimiter='#')
         headings = next(reader)
+
         if not all(field in headings for field in headers_template):
             print(f'Файл {file_name} содержит неверные заголовки')
+            continue
 
-        # Читаем ряды со 2-го
-        # какой элемент ряда к какому заголовку к какому заголовку относится
+        print(f'Работаем с файлом: {colored(file_name, "cyan")}')
+        print()
+        print('Читаем данные:')
+
+        for row in reader:
+            formatted_data = {}
+            for header, idx in headers_template.items():
+                idx = headers_template[header]
+                value = row[idx] if idx < len(row) else ''
+                color = colors[idx]
+                formatted_data[header] = colored(f'{header}: {value}', color)
+
+
+            for header in headers_template:
+                print(formatted_data[header], end=' ')
+
+            print()
+
+
+
+
+
+        #сохранить файл: with open('result.scv'), mode='w', encoding='utf-8') as file:
+        #    writer = csv.writer(file, delimiter='#')
+        #    writer.writerows(formatted_data)
+        # какой элемент ряда к какому заголовку относится
         # меняю порядок в соответствии с шаблоном заголовков
         # добавляем упорядоченный ряд в копилку
